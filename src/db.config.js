545fdeea3ee76de44aa95edf10106d3373b39cdc
teo.dor <teo.dor@kakaocorp.com>
2019-09-19 12:31:39 +0900

    add option in mongoose

--- src/db.config.js
+++ src/db.config.js
@@ -5,5 +5,5 @@ mongoose.set('useCreateIndex', true);
 
 const url = 'mongodb+srv://admin:Teo.dor1024@doeatrecord-cgmwf.mongodb.net/real?retryWrites=true&w=majority';
 
-mongoose.connect(url, {useNewUrlParser: true});
+mongoose.connect(url, {useNewUrlParser: true, useUnifiedTopology: true});
 mongoose.connection.once('open', () => console.log(`Connected to mongo at ${url}`));
