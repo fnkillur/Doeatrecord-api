151b4a9e6e5018e73b51b0bf3b24f1c33aa68812
태영 <fnkillur@gmail.com>
2019-12-08 14:17:52 +0900

    Matching alarm 컬럼 기본값 변경

--- src/models/Matching.js
+++ src/models/Matching.js
@@ -10,7 +10,7 @@ const MatchingSchema = new Schema({
 	type: String,
 	completed: {type: Boolean, default: false},
 	result: {type: String, default: 'wait'},
-	alarm: {type: Boolean, default: false},
+	alarm: {type: Boolean, default: true},
 	created: {type: Date, default: Date.now},
 	updated: {type: Date, default: Date.now}
 });
