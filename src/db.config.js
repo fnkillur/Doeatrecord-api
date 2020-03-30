ed14f0764e4ba31426f6769325c918335e6b65bf
teo <teo.dor@kakaocorp.com>
2020-03-30 20:31:47 +0900

    설정 리얼로 변경

--- src/db.config.js
+++ src/db.config.js
@@ -3,8 +3,8 @@ import mongoose from 'mongoose';
 mongoose.Promise = global.Promise;
 mongoose.set('useCreateIndex', true);
 
-// const url = 'mongodb+srv://admin:Teo.dor1024@doeatrecord-cgmwf.mongodb.net/real?retryWrites=true&w=majority';
-const url = 'mongodb+srv://admin:Teo.dor1024@doeatrecord-cgmwf.mongodb.net/test?retryWrites=true&w=majority';
+const url = 'mongodb+srv://admin:Teo.dor1024@doeatrecord-cgmwf.mongodb.net/real?retryWrites=true&w=majority';
+// const url = 'mongodb+srv://admin:Teo.dor1024@doeatrecord-cgmwf.mongodb.net/test?retryWrites=true&w=majority';
 
 mongoose.connect(url, {useNewUrlParser: true});
 mongoose.connection.once('open', () => console.log(`Connected to mongo at ${url}`));
