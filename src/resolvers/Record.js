6c14b7bd8c68251ee6e479a153cb386ac66c5761
teo.dor <teo.dor@kakaocorp.com>
2021-02-01 17:15:44 +0900

    로그 찍기

--- src/resolvers/Record.js
+++ src/resolvers/Record.js
@@ -1,6 +1,7 @@
 import moment from "moment";
 import User from "../models/User";
 import Record from "../models/Record";
+import {format} from "date-fns";
 
 const getAndList = async ({userId, keyword, now, coordinate}) => {
   if (!userId) {
@@ -18,6 +19,7 @@ const getAndList = async ({userId, keyword, now, coordinate}) => {
       visitedMonth: moment(now).month() + 1,
       visitedYear: moment(now).year(),
     });
+    console.log(now, moment(now).format('yyyy.MM.dd'), format(now, 'yyyy.MM.dd'));
     console.log('year: ', moment(now).year());
     console.log('month: ', moment(now).month() + 1);
   }
